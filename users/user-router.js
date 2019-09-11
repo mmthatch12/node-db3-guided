141d352e05bb11e95263f47ce6b44e7f24b13e6b
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-11 11:43:22 -0600

    first commit

--- users/user-router.js
+++ users/user-router.js
@@ -77,4 +77,17 @@ router.delete('/:id', (req, res) => {
   });
 });
 
+router.get('/:id/posts', (req, res) => {
+  const { id } = req.params
+
+  db('posts as p')
+    .join('users as u', 'u.id', '=', 'p.user_id')
+    .where({ user_id: id })
+    .then(posts => {
+      res.status(200).json(posts)
+  }).catch(error => {
+    res.status(500).json(error)
+  })
+})
+
 module.exports = router;
